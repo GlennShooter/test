03e6a76bbdda1b46341461d01ad0eca5ee4cdb87
GlennShooter <32734514+GlennShooter@users.noreply.github.com>
2018-09-13 11:55:44 +0100

    Add files via upload

--- setup.py
+++ setup.py
@@ -11,5 +11,5 @@ setup(
       name = "GoogAnn",
       version = 1.0,
       author_email = "glenn.shooter@attacat.co.uk",
-      description = "Allows user to easily query the google analytics api"
+      description = "Allows user to easily query the google analytics api",
       py_modules = ["setup.py","GoogAnn.py"])

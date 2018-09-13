7daa4e2a66c6b56e6fd85d6f28e07442bdcefdc4
GlennShooter <32734514+GlennShooter@users.noreply.github.com>
2018-09-13 11:55:03 +0100

    Add files via upload

--- setup.py
+++ setup.py
@@ -5,5 +5,11 @@ Created on Thu Sep 13 10:08:57 2018
 @author: glenn.shooter
 """
 
-from setuptools import setup, find_packages
-find_packages('requirements.txt')
+from setuptools import setup
+
+setup(
+      name = "GoogAnn",
+      version = 1.0,
+      author_email = "glenn.shooter@attacat.co.uk",
+      description = "Allows user to easily query the google analytics api"
+      py_modules = ["setup.py","GoogAnn.py"])
